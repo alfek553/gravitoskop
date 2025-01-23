1b686273e2eea8a06526273e7cdd3eb8d795f8cc
Владислав <lapin.vlad01@gmail.com>
2025-01-23 20:49:41 +0700

    fix

--- components/RulesWrapper/index.js
+++ components/RulesWrapper/index.js
@@ -1,6 +1,6 @@
 import styles from './styles.module.scss';
 import Link from 'next/link';
-
+import Image from 'next/image';
 /**
  * Компонент `RulesWrapper` отображает страницу правил игры.
  */
@@ -15,6 +15,27 @@ const RulesWrapper = () => {
             nisi voluptatum quam voluptatem laboriosam? Commodi nam aut velit fuga, quis
             nihil quisquam itaque eos tempore veniam accusantium porro cum.
           </p>
+          <Image className={styles.image + " " + styles.image_chart}
+            src={`/image/rules/work_with_chart.jpg`}
+            width={600}
+            height={200}
+            fill={false}
+            alt='карточка5'
+          />
+                    <Image className={styles.image + " " + styles.image_title}
+            src={`/image/rules/datePicker1.jpg`}
+            width={300}
+            height={400}
+            fill={false}
+            alt='карточка4'
+          />
+                    <Image className={styles.image + " " + styles.image_title}
+            src={`/image/rules/datePicker2.jpg`}
+            width={300}
+            height={400}
+            fill={false}
+            alt='карточка3'
+          />
           <p className={styles.textContent}>    Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati voluptates
             nisi voluptatum quam voluptatem laboriosam? Commodi nam aut velit fuga, quis
             nihil quisquam itaque eos tempore veniam accusantium porro cum.
